10afe2f8c811b950482f5a4ba4b64482301d4972
Oniel-Plaza <oniel.plaza@upr.edu>
2021-02-04 09:39:17 -0400

    Did some changes to code.py

--- code.py
+++ code.py
@@ -1,1 +1,5 @@
 # This is a normal Python file just to test it and see what happens to the repo and VSCode :')
+# print("This is pretty cool!!")
+# for i in string:
+    # print(i)
+    # print(i, "this is the current iteration number.")
