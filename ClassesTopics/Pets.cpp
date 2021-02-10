aa16ee6f0c4ca0da0f49c4b066f28a1e7637fd4a
Oniel-Plaza <oniel.plaza@upr.edu>
2021-02-10 16:18:56 -0400

    added emojis to Pets.cpp

--- ClassesTopics/Pets.cpp
+++ ClassesTopics/Pets.cpp
@@ -5,11 +5,22 @@ using namespace std;
 
 int main() {
     Pets p1 = Pets("Dog", "Chris", "Black", "Small");
+    Pets p2 = Pets("Cat", "Bubbles", "White", "Medium");
+    
 
-    for(int i = 0; i < 3; i++) {
-        cout << p1.getSpecies() << endl;
-        cout << p1.getColor() << endl;
-        cout << p1.getName() << endl;
-        cout << p1.getSize() << endl;
+    for(int i = 0; i < 3; i++) 
+    {
+        if (i == 0) {   
+            cout << endl << p1.getSpecies() << "🐶🐶" << endl;
+            cout << p1.getColor() << endl;
+            cout << p1.getName() << endl;
+            cout << p1.getSize() << endl << endl;
+        }
+        else if (i >= 1) {
+            cout << p2.getSpecies() << "🐱🐱" << endl;
+            cout << p2.getColor() << endl;
+            cout << p2.getName() << endl;
+            cout << p2.getSize() << endl << endl;
+        }
     }
 }
